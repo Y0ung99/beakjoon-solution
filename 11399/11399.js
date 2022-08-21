d705eeeeb7bea2b5dfaf35a43405976fc48d6a59
Y0ung99 <ancolor@knou.ac.kr>
2022-08-22 00:33:42 +0900

    1541, 11399 제목 추가

--- 11399/11399.js
+++ 11399/11399.js
@@ -1,3 +1,4 @@
+// ATM
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString();
--- 1541/1541.js
+++ 1541/1541.js
@@ -1,3 +1,4 @@
+// 잃어버린 괄호
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString();
